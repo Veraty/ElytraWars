a9363dfbaf79b167f471e91b8341085f2b8fac77
BuildTools <unconfigured@null.spigotmc.org>
2017-12-16 17:56:29 +0100

    + Added SqlConnection.java

---
+++ src/main/java/de/veraty/elytrawars/database/sql/SqlConnection.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (C) 2017 merlin
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.veraty.elytrawars.database.sql;
+
+import de.veraty.elytrawars.ElytraWarsPlugin;
+import de.veraty.elytrawars.database.Credential;
+import de.veraty.elytrawars.database.DatabaseConnection;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import lombok.Getter;
+
+/**
+ *
+ * @author merlin
+ */
+@Getter
+public class SqlConnection implements DatabaseConnection {
+
+    private static final String URL = "jdbc:/%s/%s";
+
+    private Connection connection;
+
+    @Override
+    public void open(Credential credential) {
+        try {
+            this.connection = DriverManager.getConnection(String.format(URL), credential.getUser(), credential.getPassword());
+        } catch (SQLException exception) {
+            ElytraWarsPlugin.getInstance().handle(exception);
+        }
+    }
+
+    @Override
+    public void close() {
+        if (isOpen()) {
+            try {
+                this.connection.close();
+            } catch (SQLException exception) {
+                ElytraWarsPlugin.getInstance().handle(exception);
+            }
+        }
+    }
+
+    @Override
+    public boolean isOpen() {
+        try {
+            return this.connection != null && !this.connection.isClosed();
+        } catch (SQLException exception) {
+            ElytraWarsPlugin.getInstance().handle(exception);
+        }
+        return false;
+    }
+
+}
